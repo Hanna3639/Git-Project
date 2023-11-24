f2353a69722848e83e59814951b01b36395342a1
hanna <yunche65@naver.com>
2023-11-24 11:24:54 +0900

    기능1

--- src/GitMain.java
+++ src/GitMain.java
@@ -3,8 +3,10 @@ public class GitMain {
 	public static void main(String[] args) {
 		System.out.println("HelloGit!!");
 		//회원가입 기능 추가 완료
+		//로그인 기능 추가 완료
 	}
 	public void join() {
 		//회원가입 수정
+		//기능1
 	}
 }
